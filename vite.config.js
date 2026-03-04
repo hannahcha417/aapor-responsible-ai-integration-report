f8db8122d359ddc6686dc691c9237fadbf8cbd29
hannahcha417 <hcha417@stanford.edu>
2026-03-04 14:35:36 -0500

    Change branch deployment

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/aapor-responsible-ai-integration-report/",
+  build: {
+    outDir: "docs",
+  },
 });
